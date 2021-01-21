b872d9d8866dc933c2ec79453048718f81c73cc3
Takshika Jambhule (Group) <takshika.jambhule@global.ntt>
2021-01-21 23:38:11 +0530

    test

--- scripts/update_hosts.py
+++ scripts/update_hosts.py
@@ -16,7 +16,7 @@ def update_hosts(Stack_Name):
         elif resource['ResourceType'] == 'AWS::EC2::Instance' and resource['ResourceStatus'] == 'CREATE_COMPLETE':
             instance = ec2.Instance(resource['PhysicalResourceId'])
             print instance.public_ip_address
-            key_pair = instance.KeyPair('test')
+            key_pair = ec2.KeyPair('test')
             print key_pair
             hosts.write('\n')
             hosts.write(instance.public_ip_address)
